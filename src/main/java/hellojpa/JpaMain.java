f811905d3d4de212825df26c99b6931cef884c2d
kdo_1999/mac <kdo_1999@naver.com>
2023-07-09 19:11:11 +0900

    단방향 연관관계, 양방향 연관관계, 사용시 주의사항

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,26 +16,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member();
-            member1.setUsername("A");
 
-            Member member2 = new Member();
-            member2.setUsername("B");
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            Member member3 = new Member();
-            member3.setUsername("C");
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
-            System.out.println("==============");
+            team.addMember(member);
 
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
+//            em.flush();
+//            em.clear();
 
-            System.out.println("member1.getId() = " + member1.getId());
-            System.out.println("member2.getId() = " + member2.getId());
-            System.out.println("member3.getId() = " + member3.getId());
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
 
-            System.out.println("==============");
+            System.out.println("==================");
+            System.out.println("members = " + findTeam);
+            System.out.println("==================");
 
             tx.commit();
         } catch (Exception e) {
